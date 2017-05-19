f6102fdd59b0627feec6d8a5cf86df5073e836a3
alumno <alumno@LAB7-5>
2017-05-19 09:16:31 -0600

    Agregar archivos de proyecto.

---
+++ ProyecoFinal/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyecoFinal.Startup))]
+namespace ProyecoFinal
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
